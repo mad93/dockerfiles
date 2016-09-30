5781b758bb684be3d477e5817e853bedeece1c49
Marc Serra <mad93@majomo.com>
2016-09-30 21:17:42 +0200

    Forcing webhook

--- mysql/mysql-setup.sh
+++ mysql/mysql-setup.sh
@@ -11,3 +11,4 @@ sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my
 /usr/sbin/mysqld &
 sleep 5
 echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+
